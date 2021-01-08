8c6967226d832ae39706c916fd0049b82cf349d8
Nabil Pervez <perveznabil@gmail.com>
2021-01-07 21:08:54 -0600

    node mongodb ex2

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert').strict;
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'nucampsite';
 
-//establish where to connect and begins connection
 MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
 
-    //checks if connection was successful - if there is no error, success
     assert.strictEqual(err, null);
 
     console.log('Connected correctly to server');
 
-    //defines the db name
     const db = client.db(dbname);
 
-    //clears the db every time this runs. this is for the practical purposes of this exercise
     db.dropCollection('campsites', (err, result) => {
         assert.strictEqual(err, null);
-        console.log('Dropped Collection', result);
+        console.log('Dropped Collection:', result);
 
-        //defines which collection
-        const collection = db.collection('campsites');
+        dboper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "Test"},
+            'campsites', result => {
+            console.log('Insert Document:', result.ops);
 
-        //inserts a record into the collection
-        collection.insertOne({name: "Breadcrumb Trail Campground", description: "Test"},
-        (err, result) => {
-            assert.strictEqual(err, null); //checks for error, if none - proceed
-            console.log('Insert Document:', result.ops); // show the result of the operation
+            dboper.findDocuments(db, 'campsites', docs => {
+                console.log('Found Documents:', docs);
 
-            collection.find().toArray((err, docs) => { // find all documents, convert found documents to an array
-                assert.strictEqual(err, null); //checks for error, if none - proceed
-                console.log('Found Documents:', docs); // logs all docs found
+                dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" },
+                    { description: "Updated Test Description" }, 'campsites',
+                    result => {
+                        console.log('Updated Document Count:', result.result.nModified);
 
-                client.close(); // immediately closes connection to db
+                        dboper.findDocuments(db, 'campsites', docs => {
+                            console.log('Found Documents:', docs);
+                            
+                            dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" },
+                                'campsites', result => {
+                                    console.log('Deleted Document Count:', result.deletedCount);
+
+                                    client.close();
+                                }
+                            );
+                        });
+                    }
+                );
             });
         });
     });
